8a2ef2afa9686aea4dd31c2143a538de5cf5604d
Eduardo Lima <duduzx02@gmail.com>
2025-06-01 05:39:17 -0300

    feat: implementa componente de clientes
    - Cria ClientesComponent standalone
    - Implementa funcionalidades:
       Listagem de clientes (getAll)
       Cadastro de novos clientes (create)
       Exclusão de clientes (delete)
    - Adiciona FormsModule para formulários
    - Configura reatividade com:
       Atualização automática da lista após operações
       Reset do formulário após cadastro

--- oficina-front/src/app/pages/clientes/clientes.component.ts
+++ oficina-front/src/app/pages/clientes/clientes.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { ClienteService, Cliente } from '../../services/cliente.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
-  templateUrl: './clientes.component.html',
-  styleUrl: './clientes.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent {
+  clientes: Cliente[] = [];
+  novoCliente: Cliente = { nome: '', telefone: '', placaVeiculo: '', modeloVeiculo: '' };
 
+  constructor(private clienteService: ClienteService) {
+    this.carregarClientes();
+  }
+
+  carregarClientes() {
+    this.clienteService.getAll().subscribe(data => this.clientes = data);
+  }
+
+  salvar() {
+    this.clienteService.create(this.novoCliente).subscribe(() => {
+      this.novoCliente = { nome: '', telefone: '', placaVeiculo: '', modeloVeiculo: '' };
+      this.carregarClientes();
+    });
+  }
+
+  deletar(id: number) {
+    this.clienteService.delete(id).subscribe(() => this.carregarClientes());
+  }
 }
