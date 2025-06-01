c2380aa0eedc6e19c8892cf2b178ddfd1714b0f2
Eduardo Lima <duduzx02@gmail.com>
2025-06-01 05:26:17 -0300

    feat: configura rotas principais do frontend
    - Define rota padrão redirecionando para /clientes
    - Mapeia rotas para componentes:
       /clientes  ClientesComponent
       /pecas  PecasComponent
    - Estrutura básica de navegação pronta

--- oficina-front/src/app/app.routes.ts
+++ oficina-front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { PecasComponent } from './pages/pecas/pecas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'clientes', pathMatch: 'full' },
+	{ path: 'clientes', component: ClientesComponent },
+	{ path: 'pecas', component: PecasComponent }
+];
