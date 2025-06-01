f45c30c4a9fb5be855f7fd96fb6c49caf8b28b21
Eduardo Lima <duduzx02@gmail.com>
2025-06-01 05:02:39 -0300

    feat: inicia frontend Angular Standalone para oficina

---
+++ oficina-front/src/app/pages/pecas/pecas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pecas',
+  imports: [],
+  templateUrl: './pecas.component.html',
+  styleUrl: './pecas.component.css'
+})
+export class PecasComponent {
+
+}
